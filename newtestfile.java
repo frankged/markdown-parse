464a4c67c2d1a8b5443facc5d5788404907fe71a
frankged <frank.drugge@gmail.com>
2022-02-02 18:13:58 -0800

    failed test

--- newtestfile.java
+++ newtestfile.java
@@ -1,3 +1,6 @@
+// 1 contributor
+//22 lines (18 sloc)  568 Bytes
+   
 import static org.junit.Assert.*;
 import org.junit.*;
 
@@ -5,7 +8,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
